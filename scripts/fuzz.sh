549c1bfeacac318a43cc53b4e1f782d2d583a543
Scott Nelson <scott@scottnelson.co>
2026-02-22 16:22:19 -0500

    Fuzzing updates (#14)

--- scripts/fuzz.sh
+++ scripts/fuzz.sh
@@ -7,7 +7,10 @@ echo "Running fuzz tests for 10 seconds each..."
 
 cargo fuzz run --fuzz-dir tests/fuzz fuzz_lcov -- -max_total_time=10
 cargo fuzz run --fuzz-dir tests/fuzz fuzz_cobertura -- -max_total_time=10
+cargo fuzz run --fuzz-dir tests/fuzz fuzz_clover -- -max_total_time=10
 cargo fuzz run --fuzz-dir tests/fuzz fuzz_jacoco -- -max_total_time=10
+cargo fuzz run --fuzz-dir tests/fuzz fuzz_istanbul -- -max_total_time=10
+cargo fuzz run --fuzz-dir tests/fuzz fuzz_gocover -- -max_total_time=10
 cargo fuzz run --fuzz-dir tests/fuzz fuzz_diff -- -max_total_time=10
 
 echo "All fuzz tests completed!"
